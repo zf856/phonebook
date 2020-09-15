f2e456992ed8bf0b9b0c8265e16e71a17e513ac3
zf851 <samin_farrokhi@yahoo.com>
2020-09-15 16:53:08 +0430

    modified db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 
-
 class db
 {
     protected $pdo;
     private $db;
     private $user;
     private $pass;
+    private $tbl;
 
     public function __construct($db='phonebook',$user='root',$pass='')
     {
@@ -18,13 +18,37 @@ class db
 
     public function connection(){
         try{
-            $this->pdo= new PDO("mysql:host=localhost;dbname={$this->db}", $this->user, $this->pass);
+            $this->pdo = new PDO("mysql:host=localhost;dbname={$this->db}", $this->user, $this->pass);
         }
-        catch (Exception $e){
+        catch(Exception $e){
             die($e->getMessage());
         }
 
     }
+    public function setTbl($tbl)
+    {
+        $this->tbl = $tbl;
+    }
+    public function selectData($data){
+        if(is_array($data)){
+            $data="'" .implode("'", $data)."'" ;
+
+        }
+    }
+    public function insertData($data){
+        if(is_array($data)){
+            $data="'" .implode("'", $data)."'" ;
+            $sql="INSERT INTO user_tbl (name,lastname) VALUES ('$data[name]','$data[lastname]')";
+
+        }
+    }
 }
 
+
+
+
+
+
 $obj=new db();
+$obj->setTbl('user_tbl');
+$obj->insert("name");
